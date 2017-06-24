8805563a69ab5a422b4ef681f3846ea927b598d1
Patrick Abbott <patrickmabbott@gmail.com>
2017-06-23 17:55:51 -0700

    Removed unsued import

--- iaroc2/src/main/java/org/jointheleague/iaroc/rest/RestResource.java
+++ iaroc2/src/main/java/org/jointheleague/iaroc/rest/RestResource.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import org.jointheleague.iaroc.Application;
 import org.jointheleague.iaroc.db.DBUtils;
 import org.jointheleague.iaroc.model.Announcements;
 import org.jointheleague.iaroc.model.EntityManager;
